3c3bcae02c59b44858c1b439ccdd38e5650bfe62
coderj001 <amirajubolchi001@gmail.com>
2025-08-28 17:32:52 +0530

    feat: create a main loop to accepted connection

--- src/Server.cpp
+++ src/Server.cpp
@@ -7,10 +7,36 @@
 #include <string>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <thread>
 #include <unistd.h>
 
+void handle_client(int client_fd) {
+  std::cout << "Client connected on thread " << std::this_thread::get_id()
+            << std::endl;
+  ;
+  char buffer[1024] = {0}; // hold data from client
+
+  while (true) {
+    ssize_t bytes_received = read(client_fd, buffer, sizeof(buffer));
+
+    if (bytes_received < 0) {
+      std::cerr << "Failed to read\n";
+    }
+
+    std::string received_data(buffer, bytes_received);
+
+    if (received_data.find("PING") != std::string::npos) {
+      const char *resp = "+PONG\r\n";
+      write(client_fd, resp, strlen(resp));
+    } else if (received_data.find("END") != std::string::npos) {
+      break;
+    }
+  }
+
+  close(client_fd);
+}
+
 int main(int argc, char **argv) {
-  // Flush after every std::cout / std::cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
@@ -42,39 +68,36 @@ int main(int argc, char **argv) {
 
   int connection_backlog = 5;
   if (listen(server_fd, connection_backlog) != 0) {
-    std::cerr << "listen failed\n";
+    std::cerr << "Listen failed\n";
     return 1;
   }
 
-  struct sockaddr_in client_addr;
-  int client_addr_len = sizeof(client_addr);
-  std::cout << "Waiting for a client to connect...\n";
-  std::cout << "Logs from your program will appear here!\n";
+  // struct sockaddr_in client_addr;
+  // int client_addr_len = sizeof(client_addr);
+  // std::cout << "Waiting for a client to connect...\n";
+  // std::cout << "Logs from your program will appear here!\n";
+  //
+  // int client_fd = accept(server_fd, (struct sockaddr *)&client_addr,
+  //                        (socklen_t *)&client_addr_len);
+  // if (client_fd < 0) {
+  //   std::cerr << "Failed to accept client connection\n";
+  // }
+  //
+  // handle_client(client_fd);
 
-  int client_fd = accept(server_fd, (struct sockaddr *)&client_addr,
-                         (socklen_t *)&client_addr_len);
-  if (client_fd < 0) {
-    std::cerr << "Failed to accept client connection\n";
-  }
-  std::cout << "Client connected\n";
-
-  char buffer[1024] = {0}; // hold data from client
-
+  // Main loop that accept connection
   while (true) {
-    ssize_t bytes_received = read(client_fd, buffer, sizeof(buffer));
+    struct sockaddr_in client_addr;
+    int client_addr_len = sizeof(client_addr);
+    int client_fd = accept(server_fd, (struct sockaddr *)&client_addr,
+                           (socklen_t *)&client_addr_len);
 
-    if (bytes_received < 0) {
-      std::cerr << "Failed to read\n";
+    if (client_fd < 0) {
+      std::cerr << "Failed to accept client connection\n";
     }
 
-    std::string received_data(buffer, bytes_received);
-
-    if (received_data.find("PING") != std::string::npos) {
-      const char *resp = "+PONG\r\n";
-      write(client_fd, resp, strlen(resp));
-    } else if (received_data.find("END") != std::string::npos) {
-      break;
-    }
+    std::thread client_thread(handle_client, client_fd); // create a new thread
+    client_thread.detach(); // let's it run independently
   }
 
   close(server_fd);
