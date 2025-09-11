b719b2f381ae51485a1259b9349bdc98b03fc7f6
Raju Ghorai <raju.ghorai@everest.enginering>
2025-09-11 12:47:36 +0530

    refactor: streamline command handling by delegating to specific functions

--- src/include/handle_command.h
+++ src/include/handle_command.h
@@ -10,3 +10,7 @@ extern std::unordered_map<std::string, StoreValue> store;
 extern std::mutex store_mutex;
 
 void handleCommand(const std::vector<std::string> &parts, int client_fd);
+void handle_set_command(const std::vector<std::string> &parts, int client_fd);
+void handle_get_command(const std::vector<std::string> &parts, int client_fd);
+void handle_echo_command(const std::vector<std::string> &parts, int client_fd);
+void handle_ping_command(int client_fd);
